26ade9c7651ebcffde90b4ecd126221794afc3af
jy870 <jy870@metanet.co.kr>
2022-01-17 13:37:43 +0900

    ~스프링 데이터 JPA

--- spring_ipmun/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ spring_ipmun/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -6,10 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.sql.DataSource;
-
 // component-scan으로 어노테이션을 이용해 controller를 제외한 @Component 요소를 자동으로 bean등록하지 않을 경우, 직접 bean 등록하는 설정파일
 // controller는 어쩔수없이 annotation 달아줘야 함
 @Configuration
@@ -20,23 +16,29 @@ public class SpringConfig {
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
 //    }
-    // jpa 위한 설정
-    private EntityManager em;
+//    // jpa 위한 설정
+//    private EntityManager em;
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+    // Spring Data JPA 위한 설정
+    private final MemberRepository memberRepository;
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+//    @Bean
+//    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
